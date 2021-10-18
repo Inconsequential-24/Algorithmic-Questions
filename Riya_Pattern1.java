e3612d27f737aec8522fba0853fa5ea8c3b876ec
Ri-yaa <92419622+Ri-yaa@users.noreply.github.com>
2021-10-18 20:42:09 +0530

    Riya_Pattern.java

---
+++ Riya_Pattern1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Pattern1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i,j,p=1;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=p;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+p++;
+}
+}
+}
