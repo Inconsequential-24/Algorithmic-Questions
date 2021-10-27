2ac5797ec4e1ba152bb5da36676afd52639e69f6
Shashwat Gupta <51000827+theHTMLprogrammer@users.noreply.github.com>
2021-10-27 17:55:04 +0530

    Delete Shashwat_Program_1.py

--- Shashwat_Gupta/Shashwat_Program_1.py
+++
@@ -1,7 +0,0 @@
-#Write a program to input a string and check if it is a palindrome or not.
-#This solution is in python programing language
-str = input("Enter the string: ")
-if str == str[::-1]:
-    print("String is Palindrome")
-else:
-    print("String is not Palindrome")
